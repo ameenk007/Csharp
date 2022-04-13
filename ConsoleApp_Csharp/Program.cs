90cb56a6954c14ee5465f2259063cb59539bc779
ameenk007 <ameenk007@gmail.com>
2022-04-13 17:02:55 +0530

    first commit

--- ConsoleApp_Csharp/Program.cs
+++ ConsoleApp_Csharp/Program.cs
@@ -6,6 +6,21 @@ namespace Dlithe
        public static void Main()
         {
             Console.WriteLine("Welcome to C# programming");
+            int a = 12;
+            Console.WriteLine("the value is:{0}",a);
+            int b = 17;
+            Console.WriteLine("the values of a & b are {0} & {1} respectively", a, b);
+            float d = 3.145f;
+            double e = 12.956734;
+            Console.WriteLine("the float value is:{0}", d);
+            Console.WriteLine("the double value is:{0}", e);
+            string c = "Bootcamp";
+            Console.WriteLine($"the value inside the string is:{c}");
+            bool f= false;
+            Console.WriteLine($"boolean value is:{f}");
+            char g= 'A';
+            Console.WriteLine($"the character is:{g}");
+            Console.WriteLine($"the character is:" + g); //we can either use {g} or concatinate g (ie; +g) to show the value in result
         }
     }
 }
