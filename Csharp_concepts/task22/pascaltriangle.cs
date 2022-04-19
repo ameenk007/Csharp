efde070914de1baa90a9cb4a647bd0d658cc67c6
ameenk007 <ameenk007@gmail.com>
2022-04-19 21:27:46 +0530

    3rd commit

---
+++ Csharp_concepts/task22/pascaltriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_concepts.task22
+{
+    internal class pascaltriangle
+    {
+        public static void Main()
+        {
+            int row, i, j, blank, c = 1;
+            Console.WriteLine("Enter the no. of rows");
+                row=Convert.ToInt32(Console.ReadLine());
+            for(i=0; i<row; i++)
+            {
+                for (blank = 1; blank <=row-i; blank++)
+                    Console.Write("   ");
+                for (j = 0; j <= i; j++)
+                    {
+                    if (j == 0 || i == 0)
+                        c = 1;
+                    else
+                        c = c * (i - j + 1) / j;
+                    Console.Write("{0}    ", c);
+                }
+                    Console.Write("\n\n");
+            }
+                
+        }
+    }
+}
