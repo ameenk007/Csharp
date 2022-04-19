c1658f4cf06248137ee9becb9b854167e17f01eb
ameenk007 <ameenk007@gmail.com>
2022-04-19 14:32:48 +0530

    2nd commit

---
+++ Csharp_concepts/sum_of_natural_no.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_concepts
+{
+    internal class sum_of_natural_no
+    {
+        public static void Main()
+        {
+            int i = 1, sum = 0;
+            while(i<50)
+            {
+                
+                sum += i;
+                i++;
+            }
+            Console.WriteLine("Sum of the first {1} natural numbers= {0}", sum, i);
+        }
+    }
+}
